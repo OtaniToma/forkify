9f29421f0c3353a3366ffbda96a5a6bd68e98292
John Smith <otani.toma@gmail.com>
2019-07-14 11:05:52 -0700

    connect controller and model

--- src/js/models/Search.js
+++ src/js/models/Search.js
@@ -2,20 +2,18 @@ import axios from 'axios';
 
 export default class Search {
   constructor(query) {
-    this.query = query; 
+    this.query = query;
   }
 
   async getResults(query) {
     const proxy = 'https://cors-anywhere.herokuapp.com/';
     const key = '9cd62e3f553d43b377db986016afc94d';
     try {
-    const res = await axios(`${proxy}https://www.food2fork.com/api/search?key=${key}&q=${this.query}`);
-    this.result = res.data.recipes;
-    console.log(recipes);
-    } catch(error) {
+      const res = await axios(`${proxy}https://www.food2fork.com/api/search?key=${key}&q=${this.query}`);
+      this.result = res.data.recipes;
+      console.log(this.result);
+    } catch (error) {
       alert(error);
     }
   }
 }
-
-
