a20040aa50fc0b844ec21a2c7bfe026348efe80b
John Smith <otani.toma@gmail.com>
2019-07-14 17:35:52 -0700

    add limitRecipeTitle

--- src/js/models/Search.js
+++ src/js/models/Search.js
@@ -7,7 +7,9 @@ export default class Search {
 
   async getResults(query) {
     const proxy = 'https://cors-anywhere.herokuapp.com/';
-    const key = '9cd62e3f553d43b377db986016afc94d';
+    // 9cd62e3f553d43b377db986016afc94d
+    // 0ed92a3fd2687e8cfaff6b449bcca923
+    const key = '0ed92a3fd2687e8cfaff6b449bcca923';
     try {
       const res = await axios(`${proxy}https://www.food2fork.com/api/search?key=${key}&q=${this.query}`);
       this.result = res.data.recipes;
